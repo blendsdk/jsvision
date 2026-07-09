8e645f606a0befc9df4a10833c38d4385f03393b
blendsdk <blendsdk@users.noreply.github.com>
2026-07-09 16:41:05 +0000

    deploy: 3e096f2039210ef03117a6cba7a853e903498c94

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.BvzSOCyH.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.B3LaW7Ln.js";import{_ as a}from"../app.CQWweLXz.js";import"./chunk-5VM5RSS4.jqexgmma.js";import"./chunk-XXDRQBXY.BfVs6Yoz.js";import"./chunk-VR4S4FIN.dYTG5ifj.js";import"./chunk-32BRIVSS.BpyhWhZS.js";import"./channel.D4wm-GuV.js";import"./framework.D_5O4tzG.js";import"./theme.CwOZ_8E1.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
