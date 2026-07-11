ae1d1203d67185ac5547ed342daf60c4d7ca409a
blendsdk <blendsdk@users.noreply.github.com>
2026-07-11 10:25:01 +0000

    Remove preview for PR 54 🛬

--- pr-preview/pr-54/assets/chunks/swimlanesDiagram-G3AALYLV.D9vFftV-.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.BEN0p4FT.js";import{_ as a}from"../app.BcwEgfqU.js";import"./chunk-5VM5RSS4.D7TknprS.js";import"./chunk-XXDRQBXY.Bc5JOnjx.js";import"./chunk-VR4S4FIN.DAChbBvK.js";import"./chunk-32BRIVSS.D_ezw8Ux.js";import"./channel.4SWPnGJ2.js";import"./framework.BZxhJ0Uw.js";import"./commonjsHelpers.Cpj98o6Y.js";import"./theme.-ErJDCwa.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
