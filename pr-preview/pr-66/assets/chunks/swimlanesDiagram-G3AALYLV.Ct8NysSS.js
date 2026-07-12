8780d3ab5a7fcc190e36a54694d20c3d9b1d9509
blendsdk <blendsdk@users.noreply.github.com>
2026-07-12 08:29:16 +0000

    Deploy preview for PR 66 🛫

---
+++ pr-preview/pr-66/assets/chunks/swimlanesDiagram-G3AALYLV.Ct8NysSS.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BJCQNQSh.js";import{_ as a}from"../app.DcCMySnH.js";import"./chunk-5VM5RSS4.BaPJ22Pr.js";import"./chunk-XXDRQBXY.Dhqd5U8D.js";import"./chunk-VR4S4FIN.etG8Zqgl.js";import"./chunk-32BRIVSS.DsE_aZUj.js";import"./channel.C7gczGkq.js";import"./framework.tf_nS-bk.js";import"./commonjsHelpers.Cpj98o6Y.js";import"./theme.Bcn9UFuv.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
