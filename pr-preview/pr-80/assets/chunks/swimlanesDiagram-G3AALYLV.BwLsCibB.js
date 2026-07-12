e5a4823940370b0b9bd3d503f02b2efd7d46ba47
blendsdk <blendsdk@users.noreply.github.com>
2026-07-12 23:07:15 +0000

    Remove preview for PR 80 🛬

--- pr-preview/pr-80/assets/chunks/swimlanesDiagram-G3AALYLV.BwLsCibB.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.PPVPoI0q.js";import{_ as a}from"../app.CPvAXK5H.js";import"./chunk-5VM5RSS4.CM9jG8s-.js";import"./chunk-XXDRQBXY.C4vxi6ny.js";import"./chunk-VR4S4FIN.C-6iwCnf.js";import"./chunk-32BRIVSS.49cvKWR-.js";import"./channel.B4WtI1cS.js";import"./framework.lC8lx-25.js";import"./theme.BILJiopu.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
