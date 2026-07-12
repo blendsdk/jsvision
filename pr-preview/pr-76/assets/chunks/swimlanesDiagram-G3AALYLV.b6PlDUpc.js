ca2154b7e0f206c8e8b48f9e73b4d7c9b270fa04
blendsdk <blendsdk@users.noreply.github.com>
2026-07-12 19:25:11 +0000

    Remove preview for PR 76 🛬

--- pr-preview/pr-76/assets/chunks/swimlanesDiagram-G3AALYLV.b6PlDUpc.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.DidRTw8g.js";import{_ as a}from"../app.MHuTofbZ.js";import"./chunk-5VM5RSS4.C8cB9bpM.js";import"./chunk-XXDRQBXY.oIBxKUPF.js";import"./chunk-VR4S4FIN.BNebHKvd.js";import"./chunk-32BRIVSS.Gx65jM-E.js";import"./channel.D_1z9Kie.js";import"./framework.D-fxZFZW.js";import"./theme.DuGDxIy0.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
