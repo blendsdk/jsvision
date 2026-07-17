9d7a2c2543a329041f38af44e8bcf908677ff310
blendsdk <blendsdk@users.noreply.github.com>
2026-07-17 20:43:10 +0000

    Deploy preview for PR 83 🛫

--- pr-preview/pr-83/assets/chunks/swimlanesDiagram-G3AALYLV.B4dAKabf.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.BbBIaG8e.js";import{_ as a}from"../app.DKwbX-9m.js";import"./chunk-5VM5RSS4.D4NGjNPS.js";import"./chunk-XXDRQBXY.BwvNVjr-.js";import"./chunk-VR4S4FIN.CUiKiT8r.js";import"./chunk-32BRIVSS.DF2_Xy5z.js";import"./channel.CTg4TnXe.js";import"./framework.BlZvSPPP.js";import"./theme.CgzHH3Ue.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
