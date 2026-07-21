32d701c9e3a04505cc137b30bb5f5e741df272f6
blendsdk <blendsdk@users.noreply.github.com>
2026-07-21 11:52:48 +0000

    Remove preview for PR 140 🛬

--- pr-preview/pr-140/assets/chunks/swimlanesDiagram-G3AALYLV.DuyzezYM.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.DbpNq4kI.js";import{_ as a}from"../app.B1gJvomg.js";import"./chunk-5VM5RSS4.Iumbh1xv.js";import"./chunk-XXDRQBXY.gmauO2hU.js";import"./chunk-VR4S4FIN.DkfPM-yf.js";import"./chunk-32BRIVSS.DTZaf7eo.js";import"./channel.DZqvKtaq.js";import"./framework.CsQc2Iik.js";import"./theme.DPiKATLg.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
