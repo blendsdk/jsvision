789135c4716c147329189ee95236d296f2f1ca54
blendsdk <blendsdk@users.noreply.github.com>
2026-07-22 10:46:12 +0000

    Deploy preview for PR 165 🛫

---
+++ pr-preview/pr-165/assets/chunks/swimlanesDiagram-G3AALYLV.CbjF43SK.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.C_G9Wa3u.js";import{_ as a}from"../app.DwsjgImI.js";import"./chunk-5VM5RSS4.Di-grrKO.js";import"./chunk-XXDRQBXY.B9Th0IH5.js";import"./chunk-VR4S4FIN.BVpGquEh.js";import"./chunk-32BRIVSS.BHKrfJuG.js";import"./channel.DZNGRZYd.js";import"./framework.BjA-8Gni.js";import"./theme.Dxuhi_vy.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
