ba1e1554d71bfa2e25e52c0293b1b680b83516ec
blendsdk <blendsdk@users.noreply.github.com>
2026-07-22 11:23:25 +0000

    Remove preview for PR 166 🛬

--- pr-preview/pr-166/assets/chunks/swimlanesDiagram-G3AALYLV.Cp-RrJGs.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.Cbgl4xtt.js";import{_ as a}from"../app.D5oyUVht.js";import"./chunk-5VM5RSS4.DND3R4sI.js";import"./chunk-XXDRQBXY.DFEaZvka.js";import"./chunk-VR4S4FIN.D5IQJQy-.js";import"./chunk-32BRIVSS.D9XvealW.js";import"./channel.CX3ut1Zq.js";import"./framework.DCFA5q_3.js";import"./theme.DsJ7-gDu.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
