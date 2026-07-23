c422b6be3eba8cd74ef6ccaf520655f41cbab74d
blendsdk <blendsdk@users.noreply.github.com>
2026-07-23 14:22:01 +0000

    Deploy preview for PR 177 🛫

--- pr-preview/pr-177/assets/chunks/channel.UKVnbMP-.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.D3Y0zNZ0.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ pr-preview/pr-177/assets/chunks/swimlanesDiagram-G3AALYLV.D0aVzPfp.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.CUS-jGug.js";import{_ as a}from"../app.vfHCTeVG.js";import"./chunk-5VM5RSS4.wBrNtqe8.js";import"./chunk-XXDRQBXY.CTlvA88q.js";import"./chunk-VR4S4FIN.OyAzzZVQ.js";import"./chunk-32BRIVSS.ion-VbLK.js";import"./channel.Cl0jyU2S.js";import"./framework.COJ7LtHI.js";import"./theme.DSmj3KB6.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
